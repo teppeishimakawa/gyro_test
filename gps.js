585b98621c9a645db47ac759d346e041c7278b09
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-15 19:06:04 +0900

    sec

---
+++ gps.js
@@ -0,0 +1,51 @@
+
+var alpha = 0, beta = 0, gamma = 0;
+
+
+if (typeof DeviceOrientationEvent.requestPermission === 'function')
+{
+// iOS 13+
+popupOpen("requestPermissionPopup");
+} else
+{
+// non iOS 13+
+window.addEventListener("deviceorientation", function(e)
+ {
+    alpha = e.alpha;  // z軸（表裏）まわりの回転の角度（反時計回りがプラス）
+    beta  = e.beta;   // x軸（左右）まわりの回転の角度（引き起こすとプラス）
+    gamma = e.gamma;  // y軸（上下）まわりの回転の角度（右に傾けるとプラス）
+ });
+}
+
+
+  var timer = window.setInterval(function()
+{
+ displayData();
+}, 33);
+
+
+
+function displayData()
+{
+    var txt = document.getElementById("txt");
+    txt.innerHTML = "alpha: " + alpha + "<br>"
+                  + "beta:  " + beta  + "<br>"
+                  + "gamma: " + gamma;
+}
+
+
+  // for ios13
+  function requestPermission()
+  {
+    DeviceOrientationEvent.requestPermission().then(response => {
+      if (response === 'granted')
+      {
+        window.addEventListener("deviceorientation", function(e)
+       {
+        alpha = e.alpha;  // z軸（表裏）まわりの回転の角度（反時計回りがプラス）
+        beta  = e.beta;   // x軸（左右）まわりの回転の角度（引き起こすとプラス）
+        gamma = e.gamma;  // y軸（上下）まわりの回転の角度（右に傾けるとプラス）
+       });
+      }
+    }).catch(console.error);
+  };
