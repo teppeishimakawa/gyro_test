208e0b61bbe2793e81a41a8bb3268148774c3abe
島川徹平 <shimakawateppei@sitetsuMac.local>
2019-10-17 11:33:47 +0900

    1041

--- gyro.js
+++ gyro.js
@@ -3,6 +3,21 @@
 var alpha = 0, beta = 0, gamma = 0;
 
 
+var getDevice =
+function(){
+    var ua = navigator.userAgent;
+    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+        return 'sp';
+    }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+        return 'tab';
+    }else{
+        return 'other';
+    }
+};
+
+console.log(getDevice);
+
+
 // iOS 13+の場合
 //ios13はDeviceOrientationEvent.requestPermissionがfunctionとして用意されてる
 if(typeof DeviceOrientationEvent.requestPermission === 'function')
